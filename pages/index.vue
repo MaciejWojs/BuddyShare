3ace8fd741dbab2469efc5159e1bf6411de90493
MaciejWojs <misiek111403@gmail.com>
2025-03-10 21:39:32 +0100

    basic layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <p>INDEX PRZETEST</p>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col v-for="n in 6" :key="n" cols="12" md="3">
+        <v-card>
+          <!-- <v-img src="/placeholder.png" height="150px"></v-img> -->
+          <v-card-title>Popularne Video</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-<style></style>
-
-<script setup lang="ts"></script>
---
+++ pages/login.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12" md="4">
+          <v-card>
+            <v-card-title>Logowanie</v-card-title>
+            <v-card-text>
+              <v-text-field label="Email" v-model="email"></v-text-field>
+              <v-text-field label="Hasło" type="password" v-model="password"></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn @click="login">Zaloguj się</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const email = ref('');
+  const password = ref('');
+  
+  const login = () => {
+    console.log(`Logowanie: ${email.value}`);
+  };
+  </script>
+  
