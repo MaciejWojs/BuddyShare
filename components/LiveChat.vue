93bd5a40f7c0d932b5d2442560a49e692980ba9a
MaciejWojs <misiek111403@gmail.com>
2025-04-05 17:43:16 +0200

    Update dependencies and clean up unused packages in package.json and bun.lock

--- components/LiveChat.vue
+++ components/LiveChat.vue
@@ -135,7 +135,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps, defineEmits, computed } from 'vue';
+import { ref, computed } from 'vue';
 
 interface ChatMessage {
   user: string;
